7bbaebde74cb659b89845fbf2bb04d28c00cb220
Bryan Lopez <bryanlopezrr@gmail.com>
2019-11-16 14:32:32 -0500

    viewing images and choosing them, ability to sign in and sign up

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Bryan Lopez on 11/16/19.
+//  Copyright © 2019 Bryan Lopez CP. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
